fa66708bdde0ca0c5fc0e7f1eaf1e61907c494e5
Kevin Bond <kevinbond@gmail.com>
2020-06-19 14:48:41 -0400

    [feature] autoconfigure Story services:
    - add test to ensure stories as services are loaded correctly

---
+++ tests/Fixtures/Stories/ServiceStory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Zenstruck\Foundry\Tests\Fixtures\Stories;
+
+use Zenstruck\Foundry\Story;
+use Zenstruck\Foundry\Tests\Fixtures\Factories\PostFactory;
+use Zenstruck\Foundry\Tests\Fixtures\Service;
+
+/**
+ * @author Kevin Bond <kevinbond@gmail.com>
+ */
+final class ServiceStory extends Story
+{
+    private Service $service;
+
+    public function __construct(Service $service)
+    {
+        $this->service = $service;
+    }
+
+    public function build(): void
+    {
+        $this->add('post', PostFactory::new()->create(['title' => $this->service->name]));
+    }
+}
