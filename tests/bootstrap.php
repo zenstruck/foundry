8dd15082aa26bf3220b5bf092f92317559cfaaf8
Kevin Bond <kevinbond@gmail.com>
2020-06-19 14:24:20 -0400

    [minor] remove tmp cache dir before running tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,12 @@
 <?php
 
+use Symfony\Component\Filesystem\Filesystem;
 use Zenstruck\Foundry\Test\GlobalState;
 use Zenstruck\Foundry\Tests\Fixtures\Stories\TagStory;
 
 require \dirname(__DIR__).'/vendor/autoload.php';
+
+(new Filesystem())->remove(\sys_get_temp_dir().'/zenstruck-foundry');
 
 GlobalState::add(static function() {
     TagStory::load();
