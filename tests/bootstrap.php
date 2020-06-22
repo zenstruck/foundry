c4aa0465d38f17161dbffaab41666082d50d6ba6
Kevin Bond <kevinbond@gmail.com>
2020-06-22 07:13:51 -0400

    [minor] rename Test/Configuration to Test/TestState:
    - using without bundle must be explicitly configured

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,11 +2,16 @@
 
 use Symfony\Component\Filesystem\Filesystem;
 use Zenstruck\Foundry\Test\GlobalState;
+use Zenstruck\Foundry\Test\TestState;
 use Zenstruck\Foundry\Tests\Fixtures\Stories\TagStory;
 
 require \dirname(__DIR__).'/vendor/autoload.php';
 
 (new Filesystem())->remove(\sys_get_temp_dir().'/zenstruck-foundry');
+
+if (!\getenv('USE_FOUNDRY_BUNDLE')) {
+    TestState::withoutBundle();
+}
 
 GlobalState::add(static function() {
     TagStory::load();
