018e4ce3b4262e5576fedb54c6279a0a5bc9fb6d
Kevin Bond <kevinbond@gmail.com>
2020-06-22 11:27:10 -0400

    [feature] add bundle config

--- src/Bundle/DependencyInjection/ZenstruckFoundryExtension.php
+++ src/Bundle/DependencyInjection/ZenstruckFoundryExtension.php
@@ -5,23 +5,61 @@ namespace Zenstruck\Foundry\Bundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
 use Zenstruck\Foundry\Story;
 
 /**
  * @author Kevin Bond <kevinbond@gmail.com>
  */
-final class ZenstruckFoundryExtension extends Extension
+final class ZenstruckFoundryExtension extends ConfigurableExtension
 {
-    public function load(array $configs, ContainerBuilder $container)
+    protected function loadInternal(array $mergedConfig, ContainerBuilder $container): void
     {
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
 
-        $loader->load('makers.xml');
         $loader->load('services.xml');
 
         $container->registerForAutoconfiguration(Story::class)
             ->addTag('foundry.story')
         ;
+
+        $this->configureFaker($mergedConfig['faker'], $container);
+        $this->configureDefaultInstantiator($mergedConfig['instantiator'], $container);
+    }
+
+    private function configureFaker(array $config, ContainerBuilder $container): void
+    {
+        if ($config['service']) {
+            $container->setAlias('zenstruck_foundry.faker', $config['service']);
+
+            return;
+        }
+
+        if ($config['locale']) {
+            $container->getDefinition('zenstruck_foundry.faker')->addArgument($config['locale']);
+        }
+    }
+
+    private function configureDefaultInstantiator(array $config, ContainerBuilder $container): void
+    {
+        if ($config['service']) {
+            $container->setAlias('zenstruck_foundry.default_instantiator', $config['service']);
+
+            return;
+        }
+
+        $definition = $container->getDefinition('zenstruck_foundry.default_instantiator');
+
+        if ($config['without_constructor']) {
+            $definition->addMethodCall('withoutConstructor');
+        }
+
+        if ($config['allow_extra_attributes']) {
+            $definition->addMethodCall('allowExtraAttributes');
+        }
+
+        if ($config['always_force_properties']) {
+            $definition->addMethodCall('alwaysForceProperties');
+        }
     }
 }
