36f749dd49e13436039ad1783456cd7da0275575
Jan Schädlich <schaedlich.jan@gmail.com>
2021-05-15 14:21:19 +0100

    Apply code review changes

--- src/Bundle/Command/StubCommand.php
+++ src/Bundle/Command/StubCommand.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 namespace Zenstruck\Foundry\Bundle\Command;
 
 use Symfony\Component\Console\Command\Command;
--- src/Bundle/Command/StubMakeStory.php
+++ src/Bundle/Command/StubMakeStory.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 namespace Zenstruck\Foundry\Bundle\Command;
 
 final class StubMakeStory extends StubCommand
