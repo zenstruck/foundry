4b603d4882d190d704fec1aefd52ab13741829e0
benjaminknecht <knecht.benjamin@yahoo.de>
2021-12-29 23:20:42 +0100

    csfixer

--- tests/Fixtures/Entity/EntityWithRelations.php
+++ tests/Fixtures/Entity/EntityWithRelations.php
@@ -2,8 +2,6 @@
 
 namespace Zenstruck\Foundry\Tests\Fixtures\Entity;
 
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
