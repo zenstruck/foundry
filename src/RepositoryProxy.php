fba0933fc8a675a6d19ef51e8ab031fb8c795d5b
Nicolas <39097145+nicolasne@users.noreply.github.com>
2023-03-21 14:30:53 +0100

    fix: regression from embedded object in findOrCreate function (#438)
    * fix: regression from embedded object in findOrCreate function
    
    * adjust exception message
    
    ---------
    
    Co-authored-by: Nicolas Nénon <nicolas.nenon@ekino.com>
    Co-authored-by: Kevin Bond <kevinbond@gmail.com>

--- src/RepositoryProxy.php
+++ src/RepositoryProxy.php
@@ -326,12 +326,14 @@ final class RepositoryProxy implements ObjectRepository, \IteratorAggregate, \Co
 
             if ($attributeValue instanceof Factory) {
                 $attributeValue = $attributeValue->withoutPersisting()->create()->object();
+            } else if ($attributeValue instanceof Proxy) {
+                $attributeValue = $attributeValue->object();
             }
 
             try {
                 $metadataForAttribute = $this->getObjectManager()->getClassMetadata($attributeValue::class);
             } catch (MappingException $e) {
-                throw new \InvalidArgumentException('Only embeddable objects can be passed as attributes for "findOrCreate()" method.', previous: $e);
+                throw new \InvalidArgumentException('Only managed objects can be passed as attributes for "findOrCreate()" method.', previous: $e);
             }
 
             $isEmbedded = match ($metadataForAttribute::class) {
@@ -340,8 +342,11 @@ final class RepositoryProxy implements ObjectRepository, \IteratorAggregate, \Co
                 default => throw new \LogicException(\sprintf('Metadata class %s is not supported.', $metadataForAttribute::class))
             };
 
+            // it's a regular entity
             if (!$isEmbedded) {
-                throw new \InvalidArgumentException('Only embeddable objects can be passed as attributes for "findOrCreate()" method.');
+                $normalizedCriteria[$attributeName] = $attributeValue;
+
+                continue;
             }
 
             foreach ($metadataForAttribute->getFieldNames() as $field) {
