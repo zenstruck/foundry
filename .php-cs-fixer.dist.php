bd064053f2c2a40ad72bb9ae0475dda098d2a7ac
nikophil <nikophil@users.noreply.github.com>
2023-11-07 17:24:43 +0000

    bot: sync with template [skip ci]

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,11 +1,5 @@
 <?php
 
-$finder = PhpCsFixer\Finder::create()
-    ->in([__DIR__.'/src', __DIR__.'/tests'])
-    ->notName('*.tpl.php')
-    ->exclude('Fixtures/')
-;
-
 $file = __DIR__.'/.php-cs-fixer.temp.php';
 
 \file_put_contents(
