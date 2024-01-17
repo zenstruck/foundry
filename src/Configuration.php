b868a6675b5b90f64ea69616d809a3a35e53720f
nikophil <nikophil@users.noreply.github.com>
2024-01-17 07:54:40 +0000

    bot: fix cs [skip ci]

--- src/Configuration.php
+++ src/Configuration.php
@@ -32,7 +32,7 @@ final class Configuration
 
     private ModelFactoryManager $factories;
 
-    private \Faker\Generator $faker;
+    private Faker\Generator $faker;
 
     /** @var callable */
     private $instantiator;
