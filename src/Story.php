1b67eb067a04493eeaefa79b7626e79adf5917f9
Brewal RENAULT <brewal.renault@gmail.com>
2024-10-02 15:20:42 +0200

    fix: removed no more needed argument.templateType phpstan ignore

--- src/Story.php
+++ src/Story.php
@@ -141,9 +141,9 @@ abstract class Story
             $isProxy = $this->state[$name] instanceof Proxy;
 
             $unwrappedObject = ProxyGenerator::unwrap($this->state[$name]);
-            Configuration::instance()->persistence()->refresh($unwrappedObject, force: true); // @phpstan-ignore argument.templateType
+            Configuration::instance()->persistence()->refresh($unwrappedObject, force: true);
 
-            return $isProxy ? ProxyGenerator::wrap($unwrappedObject) : $unwrappedObject; // @phpstan-ignore argument.templateType
+            return $isProxy ? ProxyGenerator::wrap($unwrappedObject) : $unwrappedObject;
         } catch (PersistenceNotAvailable|NoPersistenceStrategy|RefreshObjectFailed) {
             return $this->state[$name];
         }
