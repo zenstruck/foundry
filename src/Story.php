40be848c256d2e9922e5d602d344247c3cbb6b2b
Brewal RENAULT <brewal.renault@gmail.com>
2024-10-02 15:55:36 +0200

    fix: revert phpstan ignore argument.templateType

--- src/Story.php
+++ src/Story.php
@@ -143,7 +143,7 @@ abstract class Story
             $unwrappedObject = ProxyGenerator::unwrap($this->state[$name]);
             Configuration::instance()->persistence()->refresh($unwrappedObject, force: true);
 
-            return $isProxy ? ProxyGenerator::wrap($unwrappedObject) : $unwrappedObject;
+            return $isProxy ? ProxyGenerator::wrap($unwrappedObject) : $unwrappedObject; // @phpstan-ignore argument.templateType
         } catch (PersistenceNotAvailable|NoPersistenceStrategy|RefreshObjectFailed) {
             return $this->state[$name];
         }
