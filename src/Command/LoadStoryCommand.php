80c0a6fc97b680bc31fd99d0978509ac522ced04
Nicolas PHILIPPE <nikophil@gmail.com>
2025-05-11 20:17:03 +0200

    feat: restore db before fixtures command

--- src/Command/LoadStoryCommand.php
+++ src/Command/LoadStoryCommand.php
@@ -15,9 +15,16 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Exception\InvalidArgumentException;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\DependencyInjection\ServiceLocator;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Zenstruck\Foundry\Configuration;
+use Zenstruck\Foundry\Exception\PersistenceNotAvailable;
+use Zenstruck\Foundry\Persistence\ResetDatabase\BeforeFirstTestResetter;
+use Zenstruck\Foundry\Persistence\ResetDatabase\ResetDatabaseManager;
 use Zenstruck\Foundry\Story;
+use Zenstruck\Foundry\Tests\Fixture\TestKernel;
 
 /**
  * @author Nicolas PHILIPPE <nikophil@gmail.com>
@@ -29,6 +36,9 @@ final class LoadStoryCommand extends Command
         private readonly ServiceLocator $stories,
         /** @var ServiceLocator<list<Story>> */
         private readonly ServiceLocator $groupedStories,
+        /** @var iterable<BeforeFirstTestResetter> */
+        private iterable $databaseResetters,
+        private KernelInterface $kernel,
     )
     {
         parent::__construct();
@@ -36,11 +46,18 @@ final class LoadStoryCommand extends Command
 
     protected function configure(): void
     {
-        $this->addArgument('name', InputArgument::OPTIONAL, 'The name of the story to load');
+        $this
+            ->addArgument('name', InputArgument::OPTIONAL, 'The name of the story to load.')
+            ->addOption('append', 'a', InputOption::VALUE_NONE, 'Skip resetting database and append data to the existing database.')
+        ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        if (!$input->getOption('append')) {
+            $this->resetDatabase();
+        }
+
         $stories = [];
 
         if (null === ($name = $input->getArgument('name'))) {
@@ -65,5 +82,12 @@ final class LoadStoryCommand extends Command
         }
 
         return self::SUCCESS;
+    }
+
+    private function resetDatabase(): void
+    {
+        foreach ($this->databaseResetters as $databaseResetter) {
+            $databaseResetter->resetBeforeFirstTest($this->kernel);
+        }
     }
 }
