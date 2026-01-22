d9e89983f4cfee47396aace3ac2192f73bcb098d
Nicolas PHILIPPE <nikophil@gmail.com>
2026-01-22 22:21:10 +0100

    feat: automatic DB reset

--- tests/Integration/Attribute/WithStory/WithStoryOnClassTest.php
+++ tests/Integration/Attribute/WithStory/WithStoryOnClassTest.php
@@ -17,9 +17,9 @@ use PHPUnit\Framework\Attributes\RequiresPhpunit;
 use PHPUnit\Framework\Attributes\RequiresPhpunitExtension;
 use PHPUnit\Framework\Attributes\Test;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Zenstruck\Foundry\Attribute\ResetDatabase;
 use Zenstruck\Foundry\Attribute\WithStory;
 use Zenstruck\Foundry\PHPUnit\FoundryExtension;
-use Zenstruck\Foundry\Test\ResetDatabase;
 use Zenstruck\Foundry\Tests\Fixture\Factories\Entity\GenericEntityFactory;
 use Zenstruck\Foundry\Tests\Fixture\Stories\EntityPoolStory;
 use Zenstruck\Foundry\Tests\Fixture\Stories\EntityStory;
@@ -32,9 +32,10 @@ use Zenstruck\Foundry\Tests\Integration\RequiresORM;
 #[RequiresPhpunit('>=11.0')]
 #[RequiresPhpunitExtension(FoundryExtension::class)]
 #[WithStory(EntityStory::class)]
+#[ResetDatabase]
 final class WithStoryOnClassTest extends KernelTestCase
 {
-    use RequiresORM, ResetDatabase;
+    use RequiresORM;
 
     #[Test]
     public function can_use_story_in_attribute(): void
