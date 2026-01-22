37d5bd3731d6409ec16758bcca8e49974bd65dce
nikophil <10139766+nikophil@users.noreply.github.com>
2026-01-22 16:35:51 +0000

    bot: fix cs [skip ci]

--- tests/Integration/DataProvider/DataProviderWithProxyPersistentEntityFactoryTest.php
+++ tests/Integration/DataProvider/DataProviderWithProxyPersistentEntityFactoryTest.php
@@ -29,7 +29,6 @@ use Zenstruck\Foundry\Tests\Fixture\Factories\Entity\GenericEntityFactory;
 use Zenstruck\Foundry\Tests\Fixture\Factories\Entity\GenericProxyEntityFactory;
 use Zenstruck\Foundry\Tests\Fixture\Model\Embeddable;
 use Zenstruck\Foundry\Tests\Fixture\Model\GenericModel;
-use Zenstruck\Foundry\Tests\Integration\RequiresORM;
 
 use function Zenstruck\Foundry\Persistence\proxy_factory;
 
