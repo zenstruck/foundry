ef87ea4647ee3251307f73544812dba293764b27
Vincent Langlet <vincentlanglet@hotmail.fr>
2026-07-09 20:15:02 +0200

    Try

--- tests/Fixture/DoctrineEvents/AsEntityListenerListener.php
+++ tests/Fixture/DoctrineEvents/AsEntityListenerListener.php
@@ -19,12 +19,25 @@ use Doctrine\ORM\Events;
 use Zenstruck\Foundry\Tests\Fixture\Entity\ChildEntityWithCascadeToEntityListener;
 use Zenstruck\Foundry\Tests\Fixture\Entity\EntityWithAsEntityListener;
 
-#[AsEntityListener(event: Events::prePersist, entity: EntityWithAsEntityListener::class)]
-#[AsEntityListener(event: Events::prePersist, entity: ChildEntityWithCascadeToEntityListener::class)]
+#[AsEntityListener(event: Events::prePersist, method: 'prePersist', entity: EntityWithAsEntityListener::class)]
+#[AsEntityListener(event: Events::postPersist, method: 'postPersist', entity: EntityWithAsEntityListener::class)]
+#[AsEntityListener(event: Events::postPersist, method: 'postPersist2', entity: ChildEntityWithCascadeToEntityListener::class)]
 final class AsEntityListenerListener
 {
-    public function prePersist(EntityWithAsEntityListener|ChildEntityWithCascadeToEntityListener $entity, PrePersistEventArgs $eventArgs): void
+    public static bool $postPersistExecuted = false;
+
+    public function prePersist(EntityWithAsEntityListener $entity, PrePersistEventArgs $eventArgs): void
     {
         $entity->name .= ' (from AsEntityListener)';
+    }
+
+    public function postPersist(EntityWithAsEntityListener $entity): void
+    {
+        static::$postPersistExecuted = true;
+    }
+
+    public function postPersist2(ChildEntityWithCascadeToEntityListener $entity): void
+    {
+        static::$postPersistExecuted = true;
     }
 }
